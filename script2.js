e451a89435eda2672af8e326bb83d8dbd7c6ef51
Briieuc <131828760+Briieuc@users.noreply.github.com>
2024-05-27 04:14:11 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+    alert("Bienvenue sur la page Spiderman ! Merci de patienter que la page charge tranquillement! ");
+  });
+
+document.getElementById('redirectButton').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
