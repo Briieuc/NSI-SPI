aad55d84c4f87837154c1059e156be358ec7a3fe
Briieuc <131828760+Briieuc@users.noreply.github.com>
2024-05-27 04:25:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     if (username === 'Monsieur' && password === '1234') {
         errorMessage.textContent = '';
-        window.location.href = 'success.html';
+        window.location.href = 'https://docs.google.com/document/d/1Jgb4vt3kryVwAIJZCTYBFMyBCRJVvuUg0znxlgEAybc/edit?usp=sharing';
     } else {
         errorMessage.textContent = 'Nom d\'utilisateur ou mot de passe incorrect';
     }
